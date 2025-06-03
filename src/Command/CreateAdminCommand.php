8fc09dcaf708ae25905b36ee44206278929c6ff3
Oussema Sferi <oussema.sferi@gmail.com>
2025-06-03 01:35:08 +0100

    authentication implemented

---
+++ src/Command/CreateAdminCommand.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Command;
+
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Console\Attribute\AsCommand;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
+#[AsCommand(
+    name: 'create:admin',
+    description: 'Create an admin user',
+)]
+class CreateAdminCommand extends Command
+{
+    public function __construct(
+        private EntityManagerInterface $entityManager,
+        private UserPasswordHasherInterface $hasher
+    )
+    {
+        parent::__construct();
+    }
+
+    protected function configure(): void
+    {
+        $this
+            ->addArgument('arg1', InputArgument::OPTIONAL, 'Argument description')
+            ->addOption('option1', null, InputOption::VALUE_NONE, 'Option description')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $user = new User();
+        $user->setEmail('admin@admin.com');
+        $user->setRoles(['ROLE_ADMIN']);
+
+        $hashedPassword = $this->hasher->hashPassword($user, 'admin');
+        $user->setPassword($hashedPassword);
+
+        $this->entityManager->persist($user);
+        $this->entityManager->flush();
+
+        $output->writeln('✅ Admin user created: admin@admin.com / admin');
+
+        return Command::SUCCESS;
+    }
+}
